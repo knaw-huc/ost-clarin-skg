c047473b1a96b94316cfc0c8230c83b8cde0ebe9
ekoi <eko.indarto@dans.knaw.nl>
2026-07-16 09:05:26 +0200

    fix product to products

--- src/ost_clairin_skg/main.py
+++ src/ost_clairin_skg/main.py
@@ -56,7 +56,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(product.router, tags=["Product"], prefix="")
+app.include_router(products.router, tags=["Product"], prefix="")
 app.include_router(root.router, prefix="")
 app.include_router(metrics.router, tags=["Metrics"], prefix="")
 
